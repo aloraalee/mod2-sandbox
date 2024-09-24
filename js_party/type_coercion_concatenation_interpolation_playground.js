f3e6b4ed9831c6f23fca5fabbf241a10721c412d
Alora Riley <alora.riley@gmail.com>
2024-09-24 16:55:48 -0600

    Finish part 2 of type coercion

--- js_party/type_coercion_concatenation_interpolation_playground.js
+++ js_party/type_coercion_concatenation_interpolation_playground.js
@@ -27,21 +27,25 @@ console.log("type_coercion_concatenation_interpolation_playground.js is running"
 
 
 // PART 2: Concatenation
-  // var brag = "I have " + quantity + " fancy" + mythicalCreature +"!"
+  var brag = "I have " + quantity + " fancy" + mythicalCreature +"!"
+      console.log(brag)
+// 2a. Ensure the quantity, mythicalCreature, and brag variables above are not commented out. 
 
-// 2a. Ensure the quantity, mythicalCreature, and brag variables above are not commented out.  Click Run.
-
-// 2b. The value of brag above is being created using concatenation. A way to link multiple values (strings, numbers, variables, etc) together into one string using the + operator.  Let's try it!
+// 2b. The value of brag above is being created using concatenation. A way to link multiple values (strings, numbers, variables, etc) together into one string using the + operator.  
+//Let's try it!
    // Declare a variable and assign it a string value of a friend's name.
+   var bestie = 'Brian'
    // Declare a variable and assign it a string value of a nice gesture.
+   var nice_gesture = 'changing your flight to attend my birthday weekend'
    // Declare a variable "gratitude" and assign it a statement of thanks that uses concatenation and your other 2 variables.  For example:  "Thank you Kayla for pointing out the food in my teeth."
-
+      var gratitude = "Hey " + bestie + ", thank you for " + nice_gesture + "!"
+      console.log(gratitude)
 // 2c. Use the variables below to make a concatenated string.
-      // var activity = 'talk'
-      // var estrangedUncle = 'Bruno'
+      var activity = 'talk'
+      var estrangedUncle = 'Bruno'
 
-
-
+      var brunoSong = "We don't " + activity + " about " +estrangedUncle +", no, no, no!"
+      console.log(brunoSong)
 
 // PART 3: Interpolation
   // var bigBrag = `I have ${quantity} VERY fancy and sophisticated${mythicalCreature}!`  
