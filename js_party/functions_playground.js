79b8b56e5899aba658c610c8a2a442b460d739bd
Alora Riley <alora.riley@gmail.com>
2024-09-26 11:53:12 -0600

    Complete nearly all practice in functions playground

--- js_party/functions_playground.js
+++ js_party/functions_playground.js
@@ -1,5 +1,5 @@
 /* To run the code in this file, run the command:
-      node js_party/functions_playground.js
+      node functions_playground.js
 */
 
 console.log("functions_playground.js is running")
@@ -14,18 +14,19 @@ function printGreeting(greeting) {
   console.log(greeting)
 }
 
-    // printGreeting('hi')  
+    printGreeting('hi')  
     
-    // printGreeting('sup')
+    printGreeting('sup')
     
-    // printGreeting('oh hello there darling!')
+    printGreeting('oh hello there darling!')
     
 // Now call the function again yourself, passing through whatever greeting you'd like.  Make sure you pass the greeting as a string (it must be in quotes).
 
-
+printGreeting('oh hey hey')
 
 // Lesson Example 2
-// The function below is not printing anything to the console via console.log().  Instead, it is using the 'return' keyword to return a value each time the function is called.  To see the values being returned from the function invocations, we can console.log() the variables that are storing those values. 
+// The function below is not printing anything to the console via console.log().  Instead, it is using the 'return' keyword to return a value each time the function is called.  
+// To see the values being returned from the function invocations, we can console.log() the variables that are storing those values. 
 //Uncomment the three console.log()s to see the values being stored in each variable.  These console logs have a helpful string to label each one so you can see it easier in the console.
 
 function captureGreeting(greeting) {
@@ -38,9 +39,9 @@ var supGreeting = captureGreeting('sup')
 
 var fancyGreeting = captureGreeting('oh hello there darling!')
 
-// console.log('hiGreeting: ', hiGreeting)
-// console.log('supGreeting: ', supGreeting)
-// console.log('fancyGreeting: ', fancyGreeting)
+console.log('hiGreeting: ', hiGreeting)
+console.log('supGreeting: ', supGreeting)
+console.log('fancyGreeting: ', fancyGreeting)
 
 
 
@@ -54,38 +55,74 @@ var captureGreetingArrowSyntax = (greeting) => {
 
 var helloGreeting = captureGreetingArrowSyntax('hello')  
 
-// console.log("helloGreeting: ", helloGreeting)
+console.log("helloGreeting: ", helloGreeting)
 
 
 // NOTE FOR THE EXERCISES BELOW: "print" or "log" both mean to use a console.log()
 
 /*  -- SECTION A: PRACTICING FUNCTIONS STRAIGHT UP -- */
 //1A. Make a function called sayName that logs your first name
+function sayName(name) {
+  var greeting = 'Oh hey hey, '
+  return greeting + name
+}
 
-
+var sayAlora = sayName('Alora')
+console.log(sayAlora)
 //2A. Edit the sayName function above to create a variable inside of the function that is equal to a greeting. Change the console log to include the greeting concatenated with your name!
 
 
 //3A. Make a function called watchShow that console logs "I am watching". Then call the logFavoriteMovie function after the console log.
-
+function watchShow() {
+  console.log("I am watching");
+  logFavoriteMovie();
+}
+console.log(watchShow)
+var logFavoriteMovie = 'Good Will Hunting'
 
 /*  --  SECTION B: FUNCTIONS WITH PARAMETERS. -- */
 //1B. Create a function takes in two numbers and logs the numbers added together. Invoke the function two times with different numbers.
+function sum(num1, num2) {
+  console.log(num1 + num2)
+}
 
+sum(3,6)
+sum(10,2)
 
 //2B. Create a function that takes in first name and last name and logs a greeting that greets somebody by first and last name. Call the function two times with different names.
 
+function greetName(firstName, lastName) {
+  console.log('Hello ' + firstName + ' ' + lastName + '!')
+}
+greetName('Alora', 'Riley')
+greetName('Sam', 'Wise')
+
+// Here is an example with interpolation:
+function greetName(firstName, lastName) {
+  console.log(`Hello ${firstName} ${lastName}!`);
+}
+
+greetName('John', 'Doe');
+greetName('Jane', 'Smith');
 
 
 /*  -- SECTION C: FUNCTIONS WITH RETURNS, ARGS and PARAMS -- */
 //1C. Make a function that takes in two numbers as arguments and returns the sum of those two numbers. 
 // Create a variable called 'total' and assign the variable's value to be an invocation of your function (dont forget to pass in 2 numbers!)  Console log 'total'
+function sumNumbers(num1, num2) {
+  return num1 + num2;
+}
 
+// Let is used here instead of var because it can be reasigned
+let total = sumNumbers(5, 7);
+console.log(total);
 
 //2C. Make a function that takes in the name of a board game and a person's name and returns a string of "Do you want to play (board game name) with (persons name)?". Invoke the function with two different board game titles and names.
 
-
-
+function playBoardGame(boardGame, name) {
+console.log(`Do you want to play ${boardGame} with ${name} ?`)
+}
+playBoardGame('Egyptian Rat Screw', 'Brian')
 //  *** CANT GET ENOUGH FUNCTION PRACTICE??  HERE'S SOME MORE!
 
 // 1D. Create a function called printInfo. This function should take in 3 arguments: `name`, `age` and `hometown`. The function should print a string that states your name, age and hometown using interpolation.
