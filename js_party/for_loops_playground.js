dcefbbb12a5081aaf5310711d58bafec11ddc6cf
Alora Riley <alora.riley@gmail.com>
2024-10-08 11:06:06 -0400

    Complete through exercises 4 on for loops playground

--- js_party/for_loops_playground.js
+++ js_party/for_loops_playground.js
@@ -1,5 +1,5 @@
 /* To run the code in this file, run the command:
-      node js_party/for_loops_playground.js
+      node for_loops_playground.js
 */
 
 console.log("for_loops_playground.js is running")
@@ -9,25 +9,28 @@ console.log("for_loops_playground.js is running")
 // Uncomment the console log in the for loop to see how the value of i changes each loop.  Note that it loops 10 times but starts at 0.
 
 for (var i = 0; i < 10; i++) {
-  // console.log(i);
+  console.log(i);
 }
 
 
 // Exercise 2
 
-// Uncomment the console log in the for loop to see how the changing value of i lets us access each element in the array one-at-a-time, loop-by-loop, using bracket notation.
+// Uncomment the console log in the for loop to see how the changing value of i lets us access each element in the array one-at-a-time, 
+// loop-by-loop, using bracket notation.
 
 var fruits = ['apples', 'oranges', 'bananas'];
 
 for (var i = 0; i < fruits.length; i++) {
-  // console.log(`I have some ${fruits[i]}`);
+  console.log(`I have some ${fruits[i]}`);
 }
 
 
 // Exercise 3
 
 // Write a simple for loop that will loop 4 times and print "I think I can." to the console.  (No array for this exercise, just looping 4 times.)
-
+for (var i = 0; i < 4; i++) {
+  console.log('I think I can');
+}
 
 
 // Exercise 4
@@ -35,6 +38,10 @@ for (var i = 0; i < fruits.length; i++) {
 var mamboTeam = ['Angela', 'Pamela', 'Sandra', 'Rita']
 
 // Write a for loop that console logs each member of the mambo team one at a time.
+for(var i = 0; i < mamboTeam.length; i++) {
+  console.log(mamboTeam[i])
+}
+
 // Expected result:
   //Angela
   //Pamela
@@ -46,7 +53,8 @@ var mamboTeam = ['Angela', 'Pamela', 'Sandra', 'Rita']
 var nums = [10, 30, 20, 40, 60, 20]
 var sum = 0
 
-// Use a for loop to add up all the numbers in the nums array.  You'll want to look at each number one at a time and add it to the sum variable. Then, after the for loop, console log sum to confirm the expect result.
+// Use a for loop to add up all the numbers in the nums array.  You'll want to look at each number one at a time and add it to the sum variable. 
+// Then, after the for loop, console log sum to confirm the expect result.
 // Expected result: 180
 
 
